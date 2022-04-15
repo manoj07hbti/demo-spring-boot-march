68124c783f5f8724339243de0048ad107283d339
60manojkumar@gmail.com <Motivated@2019>
2022-04-15 07:29:54 +0530

    Calculator

---
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+
+    //add
+
+    @RequestMapping("add")
+    public String addition(){
+
+        int a=6;
+        int b=3;
+
+        return  "Addition is: "+(a+b) ;
+    }
+
+    //sub
+    @RequestMapping("/sub")
+    public String subtraction(){
+        int a=45;
+        int b=33;
+
+       return  "Subtraction is: "+(a-b);
+    }
+
+    //divide   (a/b)
+    //multi    (a*b)
+}
